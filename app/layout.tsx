a90dfd69543c3337ade4e0c9d6ae4ce6beb7a221
Kevin Halstead <lovefirstcc728@gmail.com>
2025-05-08 14:08:39 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  metadataBase: new URL('https://postgres-starter.vercel.app'),
-  title: 'Postgres Demo',
-  description:
-    'A simple Next.js app with a Postgres database',
-}
+  metadataBase: new URL("https://postgres-starter.vercel.app"),
+  title: "Kingdom Masterminds",
+  description: "Kingdom Masterminds description.",
+};
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.variable}>{children}</body>
     </html>
-  )
+  );
 }
